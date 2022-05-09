33bf2b3a111c479f6687a3b28ca74217c3d3f15e
Riya Mandal <96898236+riyamandal123@users.noreply.github.com>
2022-05-09 07:17:42 +0000

    problem#04

--- ActivitySet#1/problem#05.py
+++ ActivitySet#1/problem#05.py
@@ -1,22 +1,6 @@
 # Functions
 
 
-score = input("Enter Score: ")
 
-x=float(score)
-    
-    
-if (x>= 0.9):
-    print('A')
-elif(x>= 0.8):
-    print('B')
-elif(x>= 0.7):
-    print('C')
-elif(x>= 0.6):
-    print('D')
-elif(x< 0.6):
-    print('F')
-else:
-    print("error,please enter within the range of input")
     
     
