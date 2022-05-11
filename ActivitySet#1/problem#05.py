bb0d1505b1bf48bf1c5ff582b468e36c08a3fe96
Riya Mandal <96898236+riyamandal123@users.noreply.github.com>
2022-05-11 03:40:11 +0000

    problem#05

--- ActivitySet#1/problem#05.py
+++ ActivitySet#1/problem#05.py
@@ -1,6 +1,20 @@
 # Functions
+def computepay(h, r):
+    if h>40:
+        x=(40*r)+(h-40)*(1.5*r)
+        return x
+    else:
+        pay=h*r
+#print("returning",pay)    
+	return pay
 
-
+hrs = input("Enter Hours:")
+h=float(hrs)
+rate=input("Enter Rate:")
+r=float(rate)    
+  
+b=computepay(h,r)
+print("Pay",b)
 
     
     
