388134d41ac121c536792b52e4624c0193f71ced
Riya Mandal <96898236+riyamandal123@users.noreply.github.com>
2022-05-15 06:08:47 +0000

    problem#09

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,16 @@
 # Lists
 
 filename = "dataset/romeo.txt"
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+for line in fh:
+    words=line.split()
+    for word in words:
+        if word in lst:
+            continue
+        if word not in lst:
+        	lst.append(word)
+print(sorted(lst))            
+            
+        
