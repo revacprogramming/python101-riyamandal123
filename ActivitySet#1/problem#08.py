bc374adae13bf9eaa426fe285aebb17ab8953821
Riya Mandal <96898236+riyamandal123@users.noreply.github.com>
2022-05-18 17:38:02 +0000

    problem#11

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,18 +1,18 @@
 # Files
 # Use the file name mbox-short.txt as the file name
 fname = input("Enter file name: ")
-fh = open(fname)
+fh =open(fname)
 count=0
 total=0
 for line in fh:
-    if not line.startswith("X-DSPAM-Confidence: 0.8475"):
+    if not line.startswith("From:"):
         continue
-        y=line.find('0:')
+        y=line.find(':')
         #print(y)
         x=line[y+1:]
         #print(x)
         z=float(x)
-        print(z)
+        #print(z)
         count=count+1
         total=total+z
 average=total/count
--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,3 +1,23 @@
 # Tuples
 
 filename = "dataset/mbox-short.txt"
+name = input("Enter file:")
+handle = open(name)
+counts=dict()
+for line in handle:
+    l=line.strip()
+    if line.startswith("From"):
+        continue
+    l=line.split('5:')
+for hours in handle:
+    counts[hours]=counts.get(hours,0)+1
+    
+counts=None
+hours=None
+l=list()
+for count,hours in counts.items():
+	h=sorted(hours,count)
+print(h)
+
+    
+    
