ec844a3a065f3b1325e980fa867d991587bb66eb
Riya Mandal <96898236+riyamandal123@users.noreply.github.com>
2022-06-01 04:38:11 +0000

    problem#16

--- ActivitySet#1/problem#16.py
+++ ActivitySet#1/problem#16.py
@@ -1,2 +1,12 @@
 # Databases
 # https://www.py4e.com/lessons/database
+CREATE TABLE ar( 
+  name VARCHAR(128), 
+  age INTEGER
+);
+DELETE FROM ar ;
+INSERT INTO ar (name, age) VALUES ('Beatriz', 26);
+INSERT INTO ar (name, age) VALUES ('Kainui', 31);
+INSERT INTO ar(name, age) VALUES ('Janel', 22);
+INSERT INTO ar (name, age) VALUES ('Sarabeth', 30);
+SELECT hex(name || age) AS X FROM ar ORDER BY X
